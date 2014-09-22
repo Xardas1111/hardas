233709ed76746b54b677d4d8a30704f7e000ad0b
Xardas1111 <smakarchuk111@mail.ru>
2014-09-22 22:56:27 +0300

    j

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "windows.h"
+using namespace std;
+int main(){
+	int hui[20][20][20][20];
+	int n=20;
+	SYSTEMTIME lt;
+	GetLocalTime(&lt);
+	printf("First Cycle: %2d:%4d\n",lt.wSecond, lt.wMilliseconds);
+	for (int i=0;i<n*n*n*n;i++)
+	{
+		int k=i/(n*n*n);
+		int j=(i/(n*n))%n;
+		int z=(i/n)%n;
+		int i1=i%n;
+		hui[k][j][z][i1] = 0;
+	}
+	GetLocalTime(&lt);
+	printf("First Cycle: %2d:%4d\n",lt.wSecond, lt.wMilliseconds);
+	GetLocalTime(&lt);
+	printf("Second Cycle: %2d:%4d\n",lt.wSecond, lt.wMilliseconds);
+	for (int i=0;i<n;i++)
+		for (int j=0;j<n;j++)
+			for (int k=0;k<n;k++)
+				for (int z=0;z<n;z++)
+					hui[i][j][k][z] = 0;
+	GetLocalTime(&lt);
+	printf("Second Cycle: %2d:%4d\n",lt.wSecond, lt.wMilliseconds);
+	getchar();
+	return 0;
+}
